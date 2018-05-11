30c74e5673252ec00e6ae92926d89a520f026c36
Rajnish Katharotiya <katharotiyarajnish@gmail.com>
2018-05-11 15:59:55 +0530

    Create apiLIst.js

---
+++ apiLIst.js
@@ -0,0 +1,10 @@
+export const apiList = {
+    
+    postUser: { url: 'users', method: 'POST'},
+    getUser: { url: 'users', method: 'GET'},
+    deleteUser: { url: 'users', method: 'DELETE'},
+    putUser: { url: 'users', method: 'PUT'},
+
+    login: { url: '/login', method: 'POST'}
+
+};
